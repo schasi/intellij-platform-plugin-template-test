fd1d18e24e866a39524aded96d13f82d339849e9
GitHub Action <action@github.com>
2021-02-10 21:21:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/schasi/intellijplatformplugintemplatetest/MyBundle.kt
+++ src/main/kotlin/com/github/schasi/intellijplatformplugintemplatetest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.schasi.intellijplatformplugintemplatetest
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/schasi/intellijplatformplugintemplatetest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.schasi.intellijplatformplugintemplatetest.services
+
+import com.github.schasi.intellijplatformplugintemplatetest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
